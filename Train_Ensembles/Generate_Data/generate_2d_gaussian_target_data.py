e47d8f5fe6c95f8e785ceab9577e5ca1b13a7340
Giada Badaracco <gbadaracco@student.ethz.ch>
2025-10-13 17:37:04 +0200

    renamed scripts accordingly to new dataset

--- Train_Ensembles/Generate_Data/generate_2d_gaussian_target_data.py
+++ Train_Ensembles/Generate_Data/generate_2d_gaussian_target_data.py
@@ -18,11 +18,11 @@ mean_feat2, std_feat2 = 0.6, 0.4
 bkg_feat1 = np.random.normal(mean_feat1, std_feat1, n_bkg)
 bkg_feat2 = np.random.normal(mean_feat2, std_feat2, n_bkg)
 bkg_coord = np.column_stack((bkg_feat1, bkg_feat2)).astype('float32')
-'''
+
 #save target moment for coverage 
 mu_target = np.array([mean_feat1, mean_feat2], dtype=np.float32)
 np.save(os.path.join(output_dir, "mu_target.npy"), mu_target)
-'''
+
 
 # Save to files
 np.save(training_file, bkg_coord)
